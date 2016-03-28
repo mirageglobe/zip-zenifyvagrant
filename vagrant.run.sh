1f46051e6165da8aabd5d79f2516d71ee358cf68
Jimmy Mian-Guan Lim <mirageglobe@gmail.com>
2016-03-28 09:18:17 +0100

    remove rsync

--- vagrant.run.sh
+++ vagrant.run.sh
@@ -3,9 +3,6 @@
 echo "[+] Starting Vagrant Project"
 vagrant up
 
-echo "[+] Starting Vagrant Rsync"
-vagrant rsync-auto
-
 echo "[+] IP: 192.168.10.10"
 echo "[+] Browse to : http://192.168.10.10"
 
